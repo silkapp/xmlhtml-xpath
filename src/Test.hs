70425626925c8ca1ff8bc9e88d737c94e6a9ed70
Sebastiaan Visser <code@fvisser.nl>
2012-03-21 18:24:12 +0100

    Update to test case.

--- src/Test.hs
+++ src/Test.hs
@@ -2,22 +2,25 @@
 module Test where
 
 import Control.Category
+import Control.Arrow
+import Control.Arrow.List
 import Control.Arrow.ArrowF
-import Data.ByteString (ByteString)
 import Text.XmlHtml
 import Text.XmlHtml.Arrow
 import Prelude hiding ((.), id, elem)
 
 import qualified Data.ByteString as B
 
-
-
 main :: IO ()
 main =
   do file <- B.readFile "../data/test.html"
      case parseHTML "../data/test.html" file of
        Left err  -> error err
-       Right doc -> mapM_ print (run arrow (docContent doc))
+       Right doc -> mapM_ print (runListArrow (arrow . mkZ . embed) (docContent doc))
 
-  where arrow = parent . child (== "a") . mkElem "div" [] [id] . deep (child (=="a"))
+  where arrow = arr focus
+              . name
+              . siblings
+              . ancestors
+              . deep (elem (== "br"))
 
