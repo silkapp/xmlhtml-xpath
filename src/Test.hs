6d3bf021239ddcb6bb725a7d9d532325e2cbefa4
Sebastiaan Visser <code@fvisser.nl>
2012-04-16 11:31:59 +0200

    A bit more variation in the test file.

--- src/Test.hs
+++ src/Test.hs
@@ -11,14 +11,21 @@ import Prelude hiding ((.), id, elem)
 import qualified Data.ByteString as B
 
 import Text.XmlHtml.Arrow
+import Xml.XPath.Parser
 import Xml.XPath.Evaluator
 
 main :: IO ()
 main =
   do file <- B.readFile "../data/test.html"
+
+     print (parser thePath)
+
      case parseHTML "../data/test.html" file of
        Left err  -> error err
        Right doc -> mapM_ print (runListArrow (arrow . mkZ . embed) (docContent doc))
 
-  where arrow = name . evaluate "child::node()"
+  where arrow = unZ . nodeV . evaluate thePath
+        thePath = "//li[a = 'the Anchor' and a[@data-uri='nope-2']]//b"
+
+--  //permission[(canRead = 'true' or canWrite='true') and isAdmin='false']//site
 
